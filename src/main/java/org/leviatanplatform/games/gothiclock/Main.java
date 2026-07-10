9fd460844f282ab9b9c348d29add53b89bca504d
CiudadAlex <ciudad.alex@gmail.com>
2026-07-11 00:10:35 +0200

    GothicLockGameSolver

--- src/main/java/org/leviatanplatform/games/gothiclock/Main.java
+++ src/main/java/org/leviatanplatform/games/gothiclock/Main.java
@@ -1,8 +1,14 @@
 package org.leviatanplatform.games.gothiclock;
 
+import org.leviatanplatform.games.gothiclock.engine.domain.Lock;
+import org.leviatanplatform.games.gothiclock.examples.LockGenerator;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Lock lock = LockGenerator.generateDifficultLock();
+
         // FIXME finish
     }
 }
---
+++ src/main/java/org/leviatanplatform/games/gothiclock/examples/LockGenerator.java
@@ -0,0 +1,25 @@
+package org.leviatanplatform.games.gothiclock.examples;
+
+import org.leviatanplatform.games.gothiclock.engine.domain.Lock;
+
+import java.util.List;
+
+public class LockGenerator {
+
+    public static Lock generateDifficultLock() {
+
+        int positionToOpen = 3;
+        int numberOfPositions = 7;
+        List<Integer> listInitialPositions = List.of(3, 0, 6, 4, 4, 5);
+
+        Lock lock = new Lock(positionToOpen, numberOfPositions, listInitialPositions);
+
+        lock.addDependency(2, 3, true);
+        lock.addDependency(2, 5, false);
+
+        lock.addDependency(1, 2, true);
+        lock.addDependency(1, 4, false);
+
+        return lock;
+    }
+}
